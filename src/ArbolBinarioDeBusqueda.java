ecdb273a6ca77486b4547d57158e41a60eb0c5e4
lunamayoral <lunabookblog@gmail.com>
2025-03-14 12:48:54 +0100

    getaltura

--- src/ArbolBinarioDeBusqueda.java
+++ src/ArbolBinarioDeBusqueda.java
@@ -1,11 +1,14 @@
 public class ArbolBinarioDeBusqueda<K extends Comparable<K>,V> {
     NodoABB<K,V> raiz;
+
     public ArbolBinarioDeBusqueda() {
         this.raiz = null;
     }
+
     public ArbolBinarioDeBusqueda(NodoABB<K,V> raiz) {
         this.raiz = raiz;
     }
+
     public int getGrado() {
         return getGrado(raiz);
     }
@@ -23,6 +26,21 @@ public class ArbolBinarioDeBusqueda<K extends Comparable<K>,V> {
         }
         return grado;
     }
+
+    public int getAltura() {
+        return getAltura(raiz);
+    }
+
+    private int getAltura(NodoABB<K,V> nodo) {
+        if (nodo == null) {
+            return 0;
+        }
+        int alturaIzq = getAltura(nodo.getMenor());
+        int alturaDer = getAltura(nodo.getMayor());
+
+        return Math.max(alturaIzq, alturaDer);
+    }
+
     public ListaSimplementeEnlazada<T> getListaDatosNivel(nivel){
 
     }
