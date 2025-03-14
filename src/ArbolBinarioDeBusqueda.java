8c905bd463b14a2ca24de7580b1cd1069bbb31be
lunamayoral <lunabookblog@gmail.com>
2025-03-14 12:31:37 +0100

    getgrado

--- src/ArbolBinarioDeBusqueda.java
+++ src/ArbolBinarioDeBusqueda.java
@@ -1,4 +1,22 @@
 public class ArbolBinarioDeBusqueda<K extends Comparable<K>,V> {
     NodoABB<K,V> raiz;
 
+    public int getGrado() {
+        return getGrado(raiz);
+    }
+
+    private int getGrado(NodoABB<K,V> nodo) {
+        if (nodo == null) {
+            return 0;
+        }
+        int grado = 0;
+        if (nodo.getMenor() == null) {
+            grado++;
+        }
+        if (nodo.getMayor() == null) {
+            grado++;
+        }
+        return grado;
+    }
+
 }
